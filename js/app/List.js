5f52a7acae192a62031c2ffdbcccda7c44d8d65e
JanPesek <jan.pesek@am-line.com>
2019-02-12 18:56:45 +0100

    fixes...

--- js/app/List.js
+++ js/app/List.js
@@ -1,8 +1,8 @@
 //module in the same directory as shirt.js
 define(['jquery'], function($) {
   //return an object to define the "my/shirt" module
-  $.get('../BDO-Cooking/js/data/data.json', function(data) {
+  $.get('https://raw.githubusercontent.com/egwain/BDO-Cooking/master/js/data/data.json', function(data) {
     // do something with your data
-    console.log(data);
+    console.log(JSON.parse(data));
   });
 });
